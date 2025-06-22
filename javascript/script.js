29d03059498ccd1f199c41ead0431b3850e24a34
laisfsiqueira <86866@upf.br>
2025-06-22 15:51:19 -0300

    alteração das cores do site

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+function addDarkTheme(){
+    corpo.className = ""; //Limpando as classes anteriores
+    // Adiciono a classe dark, a qual ja esta criada no CSS
+    corpo.classList.add("dark"); 
+}
+function removeDarkTheme(){
+    corpo.className ="";
+    corpo.classList.add("white");
+}
+
