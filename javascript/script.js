b3c70430ab9fe622fd6772eb8f52f026e2c7f76c
izadora <frebizadora@gmail.com>
2025-06-22 23:54:38 -0300

    Acréscimo de botão de acessibilidade

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+let corpo = document.body;
+
 function addDarkTheme(){
     corpo.className = ""; //Limpando as classes anteriores
     // Adiciono a classe dark, a qual ja esta criada no CSS
@@ -8,6 +10,13 @@ function removeDarkTheme(){
     corpo.classList.add("white");
 }
 
+function addSize(){
+    corpo.classList.toggle("tamanho");
+}
 
+const formulario = document.getElementById("formulario");
 
-
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita que o formulário envie e recarregue a página
+  alert("Página em construção!🚧");
+});
